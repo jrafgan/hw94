0fcc44bc2be88340742ecdbbc6cbabbe742d71d5
Maksat Duishenaliev <maksatovy@gmail.com>
2019-05-21 14:47:30 +0600

    now track history shows only for the current user, and old date() were deleted

--- hw94_back_end/app/trackHistory.js
+++ hw94_back_end/app/trackHistory.js
@@ -7,27 +7,35 @@ const Album = require('../models/Album');
 const Artist = require('../models/Artist');
 
 router.get('/', auth, async (req, res) => {
-    await History.find().sort({datetime: -1})
-        .then(history => res.send(history))
-        .catch(() => res.sendStatus(500));
+    console.log('this is user: ', req.user);
+    try {
+        const history  = await History.find({user: req.user._id}).sort({datetime: -1});
+        res.send(history);
+    } catch (e) {
+        res.sendStatus(500);
+    }
 
 });
 
 router.post('/', auth, async (req, res) => {
+        console.log('this is user', req.user);
     try {
-        const track = await Track.find({_id: req.body.trackId});
-        const album = await Album.find({_id: track[0].album});
-        const artist = await Artist.find({_id: album[0].artist});
-        const history = new History(req.body);
-        history.date();
-        history.artistName = artist[0].name;
-        history.trackTitle = track[0].title;
+        const track = await Track.findById(req.body.trackId);
+        const album = await Album.findById(track.album);
+        const artist = await Artist.findById(album.artist);
+
+        const historyData = req.body;
+        historyData.user = req.user._id;
+        historyData.artistName = artist.name;
+        historyData.trackTitle = track.title;
+        const history = new History(historyData);
 
         await history.save();
         res.status(200).send(history);
 
-    } catch (error) {
-        return res.status(400).send(error)
+    } catch (e) {
+        console.log('this is error ', e);
+        return res.status(400).send(e)
     }
 
 });
